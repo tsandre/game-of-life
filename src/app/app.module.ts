b208487fbc637ee369d597f350f7a94a44854b2a
Tiago André <tiago.s.andre@gmail.com>
2022-06-23 17:33:47 +0100

    feat: first app structure
    World and Cell components created. Project structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WorldComponent } from './core/components/world/world.component';
+import { CellComponent } from './core/components/cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule
